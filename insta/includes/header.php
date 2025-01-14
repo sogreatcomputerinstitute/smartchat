b0f125945b8db28e4e6b423b1d5cdb6e994824f7
sogreatcomputerinstitute <destiny20202010@gmail.com>
2025-01-14 07:41:50 -0800

    Update header.php

--- insta/includes/header.php
+++ insta/includes/header.php
@@ -10,6 +10,9 @@
     <link rel="icon" href="<?php echo DIR; ?>/images/favicon/favicon.png" type="image/png">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <meta name="google-site-verification" content="BM-04UpGVtJ7Sdrgo0o1aDZxAZEK_fC4UngIIiMBdWU" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1576756667957072"
+     crossorigin="anonymous"></script>
     <!-- <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
